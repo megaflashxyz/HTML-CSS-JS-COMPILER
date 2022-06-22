4d1c3ccbbc66f77f36b6f15b29cc66f4869750ff
Megaflash <103564370+megaflashxyz@users.noreply.github.com>
2022-06-23 07:31:46 +0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,18 +38,7 @@ export default function NavBar() {
         <div className={classes.root}>
             <AppBar className={classes.appbar}>
                 <Toolbar>
-                    <Typography
-  className={classes.title}
-  variant="h6"
-  noWrap
-  component={Link}
-  to="/"
-  color="textPrimary"
->
-  <Link href="https://www.megaflash.xyz" target="_parent" underline="none">
-    HOME
-  </Link>
-</Typography>
+<Typography variant="h6" className={classes.title}>DE-CODE</Typography>
                     <IconButton onClick={handleShare}>
                         <Share/>
                     </IconButton>
